c1e07f3537ef1fd9159672c3a730a205a4e1d3a4
agostinoluca <agostino.luca89@gmail.com>
2024-02-02 18:39:01 +0100

    add site main

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,100 @@
 <script>
+import AppHeader from './components/AppHeader.vue'
+
 export default {
     name: 'App',
+    components: {
+        AppHeader
+    }
 }
 </script>
 
 <template>
-    <div id="site_header">
-        <div class="container">
-            <div class="row p-2 justify-content-between align-items-center">
-                <div class="d-flex align-items-center">
-                    <img src="./assets/img/logo.png" alt="logo DC">
-                </div>
-                <!-- /.div for Logo image -->
-                <nav>
-                    <ul class="d-flex g-1">
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">lorem</a></li>
-                        <li><a href="#">lorem</a></li>
-                    </ul>
-                </nav>
-                <!-- /nav menù Header -->
+    <AppHeader></AppHeader>
+    <main>
+        <div class="top_main">
+            <div class="container p-3">
+                <h1>Content goes here</h1>
             </div>
         </div>
-    </div>
-    <!-- /#site_header -->
+        <div class="bottom_main">
+            <div class="container p-3">
+                <div class="row">
+                    <div class="icon_nav">
+                        <div>
+                            <img src="./assets/img/digital-comics.png" alt="Digital Comics">
+                        </div>
+                        <div>
+                            <p>DIGITAL COMICS</p>
+                        </div>
+                    </div>
+                    <div class="icon_nav">
+                        <div>
+                            <img src="./assets/img/merchandise.png" alt="Digital Comics">
+                        </div>
+                        <div>
+                            <p>DC MERCHANDISE</p>
+                        </div>
+                    </div>
+                    <div class="icon_nav">
+                        <div>
+                            <img src="./assets/img/subscriptions.png" alt="Digital Comics">
+                        </div>
+                        <div>
+                            <p>DIGITAL COMICS</p>
+                        </div>
+                    </div>
+                    <div class="icon_nav">
+                        <div>
+                            <img src="./assets/img/shop-locator.png" alt="Digital Comics">
+                        </div>
+                        <div>
+                            <p>DIGITAL COMICS</p>
+                        </div>
+                    </div>
+                    <div class="icon_nav">
+                        <div>
+                            <img src="./assets/img/visa.svg" alt="Digital Comics">
+                        </div>
+                        <div>
+                            <p>DIGITAL COMICS</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </main>
 </template>
 
 <style>
-a {
-    color: var(--dc-dark);
+h1 {
+    color: var(--dc-light);
+}
+
+.top_main {
+    background-color: var(--dc-dark);
+}
+
+.bottom_main {
+    background-color: var(--dc-primary);
+    color: var(--dc-light);
+
+    & .icon_nav {
+        width: calc((100% / 5) - 2rem);
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+        margin: 1rem;
+        cursor: pointer;
+    }
+
+    & p {
+        font-size: 0.75rem;
+    }
+
+    & img {
+        max-height: 3rem;
+    }
 }
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,43 @@
+
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+
+<template>
+    <header>
+        <div class="container">
+            <div class="row p-2 justify-content-between align-items-center">
+                <div class="d-flex align-items-center">
+                    <img src="../assets/img/logo.png" alt="logo DC">
+                </div>
+                <!-- /.div for Logo image -->
+                <nav>
+                    <ul class="d-flex g-2">
+                        <li><a href="#">CHARACTERS</a></li>
+                        <li><a href="#">COMICS</a></li>
+                        <li><a href="#">MOVIES</a></li>
+                        <li><a href="#">TV</a></li>
+                        <li><a href="#">GAMES</a></li>
+                        <li><a href="#">COLLECTIBLES</a></li>
+                        <li><a href="#">VIDEOS</a></li>
+                        <li><a href="#">FANS</a></li>
+                        <li><a href="#">NEWS</a></li>
+                        <li><a href="#">SHOP</a></li>
+                    </ul>
+                </nav>
+                <!-- /nav menù Header -->
+            </div>
+        </div>
+    </header>
+    <!-- /header -->
+</template>
+
+<style scoped>
+a {
+    color: var(--dc-dark);
+    font-size: 0.75rem;
+    font-weight: 600;
+}
+</style>
